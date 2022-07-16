bbd1635eaa06efd583563b1b297cdec8751abe9a
Pabearyoga <pabearyoga@gmail.com>
2022-07-16 16:50:31 +0300

    fix task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,17 +4,11 @@ const stop = document.querySelector('[data-stop]');
 let timerColor = null;
 
 stop.setAttribute('disabled', true);
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 start.addEventListener('click', onStartClick);
 stop.addEventListener('click', onStopClick);
 
 function onStartClick() {
   start.setAttribute('disabled', true);
-
   stop.removeAttribute('disabled', false);
   timerColor = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
@@ -24,4 +18,7 @@ function onStopClick() {
   start.removeAttribute('disabled', true);
   stop.setAttribute('disabled', true);
   clearInterval(timerColor);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
