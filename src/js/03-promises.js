f2f0facb702adb9b8b3db3a6efa283cf9745223e
Pabearyoga <pabearyoga@gmail.com>
2022-07-17 01:37:18 +0300

    fix task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delaY); //first delay);
+  });
 }
+let positioN = 0;
+let amounT = 10;
+let delaY = 1000;
+
+setInterval(() => {
+  if (positioN === amounT) {
+    return;
+  }
+  positioN += 1;
+  delaY += 2000;
+  createPromise(positioN, delaY)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}, delaY);
