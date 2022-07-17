7c430a0722eba1eda584c1b0f5171a74ce1faccd
Pabearyoga <pabearyoga@gmail.com>
2022-07-17 19:15:09 +0300

    fix task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,19 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+const submit = document.querySelector('button[type="submit"]');
+let firstPosition = 1;
+let amount = null;
+let firstDelay = null;
+let delayStep = null;
+let timerId = null;
+
+form.addEventListener('input', onFormInput);
+submit.addEventListener('click', onSubmitClick);
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -9,24 +25,52 @@ function createPromise(position, delay) {
         // Reject
         reject({ position, delay });
       }
-    }, delaY); //first delay);
+    }, delay);
   });
 }
-let positioN = 0;
-let amounT = 10;
-let delaY = 1000;
 
-setInterval(() => {
-  if (positioN === amounT) {
-    return;
+function onFormInput() {
+  clearInterval(timerId);
+  onDelayInputValue();
+  onStepInputValue();
+  onAmountInputValue();
+}
+
+function onSubmitClick(event) {
+  event.preventDefault();
+  timerId = setInterval(() => {
+    if (firstPosition === amount + 1) {
+      return;
+    }
+    createPromise(firstPosition, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    firstPosition += 1;
+    firstDelay += delayStep;
+  }, amount);
+
+  firstPosition = 1;
+}
+
+function onDelayInputValue() {
+  firstDelay = Number(delayInput.value);
+  if (delayInput.value === '') {
+    firstDelay = 0;
   }
-  positioN += 1;
-  delaY += 2000;
-  createPromise(positioN, delaY)
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-}, delaY);
+}
+function onStepInputValue() {
+  delayStep = Number(stepInput.value);
+  if (stepInput.value === '') {
+    delayStep = 0;
+  }
+}
+function onAmountInputValue() {
+  amount = Number(amountInput.value);
+  if (amountInput.value === '') {
+    amount = 0;
+  }
+}
